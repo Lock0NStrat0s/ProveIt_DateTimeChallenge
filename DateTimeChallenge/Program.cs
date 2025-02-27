30b0e33e726525a90b5931908664670a417f6d34
Mohammad Jabir <mohammadj94@gmail.com>
2025-02-27 14:28:46 -0700

    completed time section.

--- DateTimeChallenge/Program.cs
+++ DateTimeChallenge/Program.cs
@@ -1,50 +1,76 @@
-﻿
-Console.WriteLine("Select date to determine how long ago it was:");
-// capture date - print how many days ago it was
-// get month, day and year from user
-bool flag = false;
-int year = 0;
-do
-{
-    Console.Write("\nYear: ");
-    int.TryParse(Console.ReadLine(), out year);
-    flag = CheckValue(year, 1, DateTime.Today.Year);
-} while (!flag);
+﻿GetDateTime();
 
-flag = false;
-int month = 0;
-do
-{
-    Console.Write("\nMonth: ");
-    int.TryParse(Console.ReadLine(), out month);
-    flag = CheckValue(month, 1, 12);
-} while (!flag);
-
-flag = false;
-int day = 0;
-do
-{
-    Console.Write("\nDay: ");
-    int.TryParse(Console.ReadLine(), out day);
-    flag = CheckValue(day, 1, DateTime.DaysInMonth(year, month));
-} while (!flag);
-
-DateTime oldDate = new DateTime(year, month, day);
-DateTime today = DateTime.Today;
-int daysAgo = (today - oldDate).Days;
-
-Console.WriteLine($"Your input date occurred {daysAgo} days ago");
-
-// capture time - print how many hours ago it was
-
-
-// allow user to select 12 or 24 hr format
-
-// allow user to select month/day/year or day/month/year format
-
-
-
-bool CheckValue(int value, int min, int max)
+static bool CheckValue(int value, int min, int max)
 {
     return value >= min && value <= max;
+}
+
+static void GetDateTime()
+{
+    Console.WriteLine("Select date to determine how long ago it was:");
+    // capture date - print how many days ago it was
+    // get month, day and year from user
+    bool flag = false;
+    int year = 2024;
+    //do
+    //{
+    //    Console.Write("\nYear: ");
+    //    int.TryParse(Console.ReadLine(), out year);
+    //    flag = CheckValue(year, 1, DateTime.Today.Year);
+    //} while (!flag);
+
+    //flag = false;
+    int month = 2;
+    //do
+    //{
+    //    Console.Write("\nMonth: ");
+    //    int.TryParse(Console.ReadLine(), out month);
+    //    flag = CheckValue(month, 1, 12);
+    //} while (!flag);
+
+    //flag = false;
+    int day = 25;
+    //do
+    //{
+    //    Console.Write("\nDay: ");
+    //    int.TryParse(Console.ReadLine(), out day);
+    //    flag = CheckValue(day, 1, DateTime.DaysInMonth(year, month));
+    //} while (!flag);
+
+    DateTime oldDate = new DateTime(year, month, day);
+    DateTime today = DateTime.Now;
+    int daysAgo = (today - oldDate).Days;
+
+    Console.WriteLine($"\nInput Date: {oldDate:D} occurred {daysAgo} days ago.");
+
+    // capture time - print how many hours ago it was
+    Console.WriteLine("\nEnter a time and see how long ago it was: ");
+
+    flag = false;
+    int hour = 14;
+    //do
+    //{
+    //    Console.Write("\nHour: ");
+    //    int.TryParse(Console.ReadLine(), out hour);
+    //    flag = CheckValue(hour, 1, 23);
+    //} while (!flag);
+
+    flag = false;
+    int minute = 15;
+    //do
+    //{
+    //    Console.Write("\nMinute: ");
+    //    int.TryParse(Console.ReadLine(), out minute);
+    //    flag = CheckValue(minute, 1, 59);
+    //} while (!flag);
+
+    DateTime oldTime = new DateTime(year, month, day, hour, minute, 0);
+    int hoursAgo = (today - oldTime).Hours;
+    int minsAgo = (today - oldTime).Minutes;
+
+    Console.WriteLine($"Your input time was {hoursAgo} hours and {minsAgo} minutes ago.");
+
+    // allow user to select 12 or 24 hr format
+
+    // allow user to select month/day/year or day/month/year format
 }
--- DateTimeChallenge/obj/Debug/net9.0/DateTimeChallenge.AssemblyInfo.cs
+++ DateTimeChallenge/obj/Debug/net9.0/DateTimeChallenge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DateTimeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ba587138b4b5625eb75ab930b8887d8a265300")]
 [assembly: System.Reflection.AssemblyProductAttribute("DateTimeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DateTimeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
